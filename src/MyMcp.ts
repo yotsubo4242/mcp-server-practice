06646938a2c0ccc61b9ac73f20991bf219af9592
yotsubo <513047.otsubo@gmail.com>
2025-05-23 12:57:32 +0900

    done

--- src/MyMcp.ts
+++ src/MyMcp.ts
@@ -1,8 +1,8 @@
-import McpAgents  from "agents/mcp"
-import McpServer from "@modelcontextprotocol/sdk"
+import { McpAgent } from "agents/mcp"
+import { McpServer } from "@modelcontextprotocol/sdk/server/mcp.js"
 import { z } from "zod"
 
-export class MyMCP extends McpAgents {
+export class MyMCP extends McpAgent {
     server = new McpServer({
         name: 'MyMCP Server',
         version: '0.1.0',
@@ -13,16 +13,19 @@ export class MyMCP extends McpAgents {
             // ツールの名前
             'dice-roll',
             // ツールの説明
-            'サイコロを振った結果を返します',
+            '指定された回数分, サイコロを振った結果を返します',
             // ツールの引数
-            { sides: z.number().min(1).max(100).default(6).describe('サイコロの面の数') },
+            {
+                sides: z.number().min(1).max(100).default(6).describe('サイコロの面の数'),
+                times: z.number().min(1).max(100).default(1).describe('振る回数'),
+            },
             // ツールの実行関数
-            async ({ sides }: { sides: number }) => {
-                const result = Math.floor(Math.random() * sides) + 1;
+            async ({ sides, times }: { sides: number, times:number }) => {
+                const results = Array.from({ length: times }, () => Math.floor(Math.random() * sides) + 1);
                 // ツールの実行結果を返す
                 // 返す値は、MCPの仕様に従って、contentプロパティを持つオブジェクトである必要がある
                 return {
-                    content: [{ type: 'text', text: result.toString() }],
+                    content: [{ type: 'text', text: results.toString() }],
                 }
             }
         )
--- src/index.ts
+++ src/index.ts
@@ -1,67 +1,24 @@
-import { DurableObject } from "cloudflare:workers";
-
-/**
- * Welcome to Cloudflare Workers! This is your first Durable Objects application.
- *
- * - Run `npm run dev` in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your Durable Object in action
- * - Run `npm run deploy` to publish your application
- *
- * Bind resources to your worker in `wrangler.jsonc`. After adding bindings, a type definition for the
- * `Env` object can be regenerated with `npm run cf-typegen`.
- *
- * Learn more at https://developers.cloudflare.com/durable-objects
- */
-
-
-/** A Durable Object's behavior is defined in an exported Javascript class */
-export class MyDurableObject extends DurableObject {
-	/**
-	 * The constructor is invoked once upon creation of the Durable Object, i.e. the first call to
-	 * 	`DurableObjectStub::get` for a given identifier (no-op constructors can be omitted)
-	 *
-	 * @param ctx - The interface for interacting with Durable Object state
-	 * @param env - The interface to reference bindings declared in wrangler.jsonc
-	 */
-	constructor(ctx: DurableObjectState, env: Env) {
-		super(ctx, env);
-	}
-
-	/**
-	 * The Durable Object exposes an RPC method sayHello which will be invoked when when a Durable
-	 *  Object instance receives a request from a Worker via the same method invocation on the stub
-	 *
-	 * @param name - The name provided to a Durable Object instance from a Worker
-	 * @returns The greeting to be sent back to the Worker
-	 */
-	async sayHello(name: string): Promise<string> {
-		return `Hello, ${name}!`;
-	}
-}
-
+import { MyMCP } from './MyMcp.js';
+ 
+// Durable Objects のエクスポート
+export { MyMCP };
+ 
 export default {
-	/**
-	 * This is the standard fetch handler for a Cloudflare Worker
-	 *
-	 * @param request - The request submitted to the Worker from the client
-	 * @param env - The interface to reference bindings declared in wrangler.jsonc
-	 * @param ctx - The execution context of the Worker
-	 * @returns The response to be sent back to the client
-	 */
-	async fetch(request, env, ctx): Promise<Response> {
-		// Create a `DurableObjectId` for an instance of the `MyDurableObject`
-		// class named "foo". Requests from all Workers to the instance named
-		// "foo" will go to a single globally unique Durable Object instance.
-		const id: DurableObjectId = env.MY_DURABLE_OBJECT.idFromName("foo");
-
-		// Create a stub to open a communication channel with the Durable
-		// Object instance.
-		const stub = env.MY_DURABLE_OBJECT.get(id);
-
-		// Call the `sayHello()` RPC method on the stub to invoke the method on
-		// the remote Durable Object instance
-		const greeting = await stub.sayHello("world");
-
-		return new Response(greeting);
+	fetch(request: Request, env: Env, ctx: ExecutionContext) {
+		const url = new URL(request.url);
+ 
+		// /sse エンドポイントの場合は SSE で応答する
+		if (url.pathname === '/sse' || url.pathname === '/sse/message') {
+			// @ts-ignore
+			return MyMCP.serveSSE('/sse').fetch(request, env, ctx);
+		}
+ 
+		// /mcp エンドポイントの場合は Streamable HTTP で応答する
+		if (url.pathname === '/mcp') {
+			// @ts-ignore
+			return MyMCP.serve('/mcp').fetch(request, env, ctx);
+		}
+ 
+		return new Response('Not found', { status: 404 });
 	},
-} satisfies ExportedHandler<Env>;
+};
